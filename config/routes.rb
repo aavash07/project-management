69d41d7aacb430a0854a3fe8e5d1ac02c54d3975
Aavash <aavash.chaudhary@gmail.com>
2023-04-02 23:24:00 +0545

    feat: complete routes for projects and contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  resource :users, only: [:create]
-  post '/login', to: 'users#login'
-  get '/auto_login', to: 'users#auto_login'
+  namespace :api do
+    namespace :v1 do
+      resources :projects do
+        collection do
+          get :my_projects
+        end
+        resources :contents, only: %i[create show index]
+      end
+      resources :contents, only: %i[update destroy]
+      post 'auth/signin', to: 'users#login'
+      post 'users/signup', to: 'users#create'
+    end
+  end
 end
