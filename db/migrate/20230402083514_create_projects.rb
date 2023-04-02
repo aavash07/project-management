624beeae14f8631e7ec560d039b4bf5bccc43dd0
Aavash <aavash.chaudhary@gmail.com>
2023-04-02 23:22:11 +0545

    feat: migration to add projects table

---
+++ db/migrate/20230402083514_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.text :description
+      t.integer :project_type
+      t.string :location
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
